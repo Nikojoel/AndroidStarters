1a7d5222370e19f4e5c6287db9646beffb909539
Nikojoel <niko-holopainen@hotmail.com>
2020-08-25 11:45:17 +0300

    notifications & material

--- app/src/main/java/com/example/helloworld2/MainActivity.kt
+++ app/src/main/java/com/example/helloworld2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.helloworld2
 import android.app.ApplicationErrorReport
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.bluetooth.BluetoothAdapter
 import android.content.Context
 import android.os.BatteryManager
 import android.os.Build
@@ -10,29 +11,37 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import com.google.android.material.snackbar.Snackbar
 
-const val CHANNEL_ID = "..."
+const val CHANNEL_ID = "123"
+const val logs = "MyLogs"
 
 class MainActivity : AppCompatActivity() {
-    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        createNotifChannel()
+
+        // Notify user if bluetooth is not on
+        if (!BluetoothAdapter.getDefaultAdapter().isEnabled) showNotification()
+
         helloButton.setOnClickListener {
             helloText.text = if (helloText.text == getString(R.string.helloText)) getString(R.string.summerText) else getString(R.string.helloText)
             showSnack(it)
-            showNotification()
+        }
+        actionButton.setOnClickListener {
+            Toast.makeText(this, getString(R.string.actionText), Toast.LENGTH_SHORT).show()
         }
     } // OnCreate Ends
 
     private fun showSnack (view: View) {
         Snackbar.make(view ,R.string.changeText, Snackbar.LENGTH_LONG).setAction(R.string.dismissText) {
-            Log.d("MyLogs", "snackbar clicked")
+            Log.d(logs, "snackbar clicked")
         }.show()
     }
 
@@ -43,7 +52,7 @@ class MainActivity : AppCompatActivity() {
                 getString(R.string.channelName),
                 NotificationManager.IMPORTANCE_DEFAULT
             ).apply {
-                description = "notif description"
+                description = getString(R.string.notifChannel)
             }
             val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE)
             as NotificationManager
@@ -52,12 +61,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showNotification() {
-        createNotifChannel()
         val notif = NotificationCompat.Builder(this, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_launcher_foreground )
-            .setContentTitle("test notification")
-            .setContentText("this is a notification")
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentTitle(getString(R.string.btOn))
+            .setContentText(getString(R.string.btText))
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
             .build()
         NotificationManagerCompat.from(this).notify(1, notif)
     }
